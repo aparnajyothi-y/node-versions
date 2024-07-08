cd410fdae3fd070daff80dae03676e9f4b8affc8
aparnajyothi-y <147696841+aparnajyothi-y@users.noreply.github.com>
2024-07-08 12:48:35 +0530

    Update Node.Tests.ps1

--- tests/Node.Tests.ps1
+++ tests/Node.Tests.ps1
@@ -34,13 +34,20 @@ Describe "Node.js" {
         $nodePath.startsWith($expectedPath) | Should -BeTrue -Because "'$nodePath' is not started with '$expectedPath'"
     }
 
-    It "cached version is used without downloading" {
-        # Analyze output of previous steps to check if Node.js was consumed from cache or downloaded
-        $useNodeLogFile = Get-UseNodeLogs
-        $useNodeLogFile | Should -Exist
-        $useNodeLogContent = Get-Content $useNodeLogFile -Raw
-        $useNodeLogContent | Should -Match "Found in cache"
-    }
+    if ($env:RUNNER_TYPE -eq "GitHub") {
+            # Analyze output of previous steps to check if Node.js was consumed from cache or downloaded
+            $useNodeLogFile = Get-UseNodeLogs
+            $useNodeLogFile | Should -Exist
+            $useNodeLogContent = Get-Content $useNodeLogFile -Raw
+            $useNodeLogContent | Should -Match "Found in cache"
+        } elseif ($env:RUNNER_TYPE -eq "self-hosted") {
+            # Get the installed version of Node.js
+            $nodeVersion = Invoke-Expression "node --version"
+            # Check if Node.js is installed
+            $nodeVersion | Should -Not -BeNullOrEmpty
+            # Check if the installed version of Node.js is the expected version
+            $nodeVersion | Should -Match $env:VERSION
+        }
 
     It "Run simple code" {
         "node ./simple-test.js" | Should -ReturnZeroExitCode
